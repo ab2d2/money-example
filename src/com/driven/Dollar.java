6cfcdab1fed5e1083d0fc6e6cb8d63565cf44dad
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-23 21:53:12 +0100

    add comment

--- src/com/driven/Dollar.java
+++ src/com/driven/Dollar.java
@@ -7,6 +7,8 @@ public class Dollar {
     }
 
     Dollar times(int multiplier) {
+        //an implication of Value Objects - which states you must return a new object in all operations
+        // this pattern removes 'aliasing' problem
         return new Dollar(amount * multiplier);
     }
 }
