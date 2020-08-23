da441c321084ee62927b0cb02f9f102a4457a8f8
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-23 22:01:33 +0100

    add comment

--- src/com/driven/DollarTest.java
+++ src/com/driven/DollarTest.java
@@ -2,7 +2,7 @@ package com.driven;
 /** Getting to green:
  * Fake it - return a constant and gradually replace with variables
  * use obvious implementation - type the most obvious implementation
- * Triangulation -
+ * Triangulation - only generalise code when we have 2 examples or more. when the second example require a more general solution, we then generalise.
  */
 
 import org.junit.jupiter.api.Test;
@@ -19,5 +19,11 @@ class DollarTest {
         assertEquals(10,product.amount);
         product = five.times(3);
         assertEquals(15,product.amount);
+    }
+
+    void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+
     }
 }
